c83eb38b1b86418b73b95977cbdb2ba2720797a2
kaspersiig <kasp9250@easv365.dk>
2019-04-25 09:16:42 +0200

    Added main.ts

---
+++ functions/src/api/main.ts
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const main = express();
+
+// Example route part 1
+// const helloWorldRouter = require('./routes/helloWorldRouter');
+
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+main.use(cors({ origin: true }));
+
+// Example route part 2
+// main.use('helloWorld', helloWorldRouter);
+
+exports.api = functions.https.onRequest(main);
