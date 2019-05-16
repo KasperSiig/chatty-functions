db076c5f152e04458f1741a08987b8b907a0ac58
kaspersiig <kasp9250@easv365.dk>
2019-05-16 08:23:17 +0200

    Perhaps now

--- functions/src/api/main.ts
+++ functions/src/api/main.ts
@@ -8,16 +8,16 @@ const main = express();
 const messageRouter = require('./routes/messageRouter');
 const filesRouter = require('./routes/filesRouter');
 const usersRouter = require('./routes/usersRouter');
-// const allowedMethods = ['POST', 'OPTIONS'];
+const allowedMethods = ['POST', 'OPTIONS'];
 
-// main.use((req, res, next) => {
-//   if (!allowedMethods.includes(req.method))
-//     return res.status(405).send('Method Not Allowed');
-//   else
-//     next();
-//
-//   return;
-// });
+main.use((req, res, next) => {
+  if (!allowedMethods.includes(req.method))
+    return res.status(405).send('Method Not Allowed');
+  else
+    next();
+
+  return;
+});
 
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
